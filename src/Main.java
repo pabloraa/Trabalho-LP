8b37384ca2ac8758732ecd5f36759109d769947d
Pablo Rangel Abreu Andrade <pabloandrade@houer.com.br>
2024-05-15 16:04:49 -0300

    tela login

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         new InterfaceGrafica();
         new LoginFrame();
+        new LoginForm(null);
     }
 }
--- src/User.java
+++ src/User.java
@@ -2,6 +2,6 @@ public class User {
     public String name;
     public String email;
     public String phone;
-    public String adress;
+    public String address;
     public String password;
 }
